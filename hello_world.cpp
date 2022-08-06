881bc1024595f7e5547cfd682db5648cca7d92e3
Shivam Dubey <Shivamdubey1740@gmail.com>
2022-08-06 11:00:07 +0530

    First C++ Program

---
+++ hello_world.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+	-> #include preprocessor is used to add extenal header files in our program.
+	-> main funtion is the starting point of any c or c++ program.
+	-> cout with << operator used to print statement. cout is object of ostream class.
+	-> endl is used to end the line.
+	-> return 0 shows that execution of program complete successfully.
+*/
+
+
+int main() {
+
+	cout<< "Hello World"<< endl;
+	return 0;
+	
+}
